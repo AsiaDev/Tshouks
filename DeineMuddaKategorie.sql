66b257c68dea52c4b30601c57b8bdb27d90b02f1
deathtoxic <andy.zhang@bbcag.ch>
2015-05-08 12:42:31 +0200

    New Kategorie
    Deine Mudda bearbeitet und schwarzer humor hinzugefügt

---
+++ DeineMuddaKategorie.sql
@@ -0,0 +1,46 @@
+Create database Tschouks;
+use Tschouks;
+
+drop table DeineMudda;
+
+CREATE TABLE DeineMudda
+(ID int not null primary key auto_increment,
+Witze varchar(200));
+
+
+Insert into DeineMudda
+(Witze)
+Values("Deine Mudda ist so fett, wenn sie vor dem Fernseher vorbeiläuft, verpasst man die ganze Herr der Ringe Trilogie")
+		,("Deine Mudda ist so fett, wenn sie vom Dach springt, singt der Nachbar 'Wenn der Mond in mein Ghetto kracht'")
+        ,("Google Earth hat angerufen, deine Mutter steht im Bild")
+        ,("Deine Mutter ist so hässlich bei ihr wird eingebrochen, um die Vorhänge zu schließen")
+		,("Deine Mudda ist so hässlich, sie arbeitet beim FBI als Blendgranate")
+        ,("Deine Mudda lacht dich aus, weil du ihr Kind bist")
+        ,("Deine Mudda hat Beine wie ein Reh. Zwar nicht so dünn, aber genau so behaart")
+        ,("Deine Mudda ist so dumm, sie leert Actimel über den Computer aus, damit er gegen Viren geschützt ist")
+        ,("Deine Mudda ist so hässlich, wenn sie sich im Spiegel ansieht, sagt der Spiegel:'Boah ich kündige'")
+        ,("Deine Mudda haut sich jeden Morgen zwei Pfannen in die Eier")
+        ,("Deine Mutter hat so lange Arschhaare, dass sie beim Klospülen runtergezogen wird")
+        ,("Deine Mudda beantragt bei Monopoly Harz 4")
+        ,("Deine Mudda kämmt sich den Rücken")
+        ,("Die Enten bewerfen deine Mudda mit Brot")
+        ,("Deine Mudda ist so fett, sie kann nicht sterben, weil der Tod nicht an sie rankommt")
+        ,("Deine Mudda bricht bei Aldi ein und vergisst zu klauen")
+        ,("Der Zoo hat angerufen, sie wollen deine Mudda wiederhaben")
+        ,("Deine Mudda arbeitet bei Weight Wachers als Vorherbild")
+        ,("Deine Mudda war als kleiner Junge schon hässlich")
+        ,("Deine Mudda ist so fett, Ebola weiss gar nicht, wo es anfangen soll")
+        ,("Deine Mudda hat Schulden beim 1 Euro Shop")
+        ,("Deine Mudda ist so haarig, wenn sie mit ihrem Hund spazieren geht, wird sie zuerst gestreichelt")
+        ,("Wenn deine Mutter mit einem grauen T-Shirt am Strand liegt, kommt Green Peace, um sie ins Wasser zu ziehen")
+        ,("Deine Mudda ist so dumm, sie macht das Radio an und fragt: 'Wer spricht da?'")
+        ,("Wenn deine Mudda sich auf den Bauch legt, kriegt sie Höhenangst")
+        ,("Deine Mudda hat Grütelgrösse Äquator")
+        ,("Deine Mudda ist so hässlich, sie macht sogar das Happy-Meal traurig")
+        ,("Deine Mudda zieht Katapulte nach Gondor")
+        ,("Deine Mudda kriegt beim Elternabend einen Klassenbucheintrag")
+        ,("Wieso lacht deine Mudda, wenn sie über die Wiese läuft? Weil das Gras ihr an den Eiern kitzelt")
+        ,("Deine Mudda steht vor eine mStop-Schild und schreit 'Die Ampel geht nicht'")
+        ;
+
+Select * from DeineMudda;
