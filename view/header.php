313ddf1a2c68999d1ab179092821002f4ba2149d
DrRiisTab <bryan.tabinas@bbcag.ch>
2015-05-13 13:52:28 +0200

    changed hole project

---
+++ view/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tshouks - Home</title>
+<link href="css/defaultStyle.css" rel="stylesheet" />
+</head>
+<body>
+	<div id="headerBar">
+		<div class="container">
+			<h1 id="header">Tshouks</h1>
+			<div id="linkBar">
+				<p>
+					<a href="#" class="active">Home</a> | <a href="#">Newest Jokes</a>
+					| <a href="#">Best Jokes</a> | <a href="#">Create new Joke</a> | <a
+						href="#">Contact</a>
+			
+			</div>
+			<div id="account">
+				<a href="index.php?type=LogIn">Log In</a> or
+				<button>Sign Up</button>
+			</div>
+		</div>
+	</div>
